69c3e0b03fa922244d4e33a8c4e86c482fbf15cc
Ly-creator <2019202338@ruc.edu.cn>
2020-06-25 10:08:36 +0800

    Third commit-2020-06-25-10:00

---
+++ startwindow.cpp
@@ -0,0 +1,6 @@
+#include "stratwindow.h"
+
+StratWindow::StratWindow(QWidget *parent) : QMainWindow(parent)
+{
+
+}
---
+++ startwindow.h
@@ -0,0 +1,17 @@
+#ifndef STRATWINDOW_H
+#define STRATWINDOW_H
+
+#include <QMainWindow>
+
+class StratWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit StratWindow(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // STRATWINDOW_H
